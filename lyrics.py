a610c285d48527c3400909eac6bcbd16bfc0fedd
Will Seymour <willseymour804@gmail.com>
2022-01-16 00:20:47 -0600

    poems.py added to extract poetry text from PoetryDB

--- lyrics.py
+++ lyrics.py
@@ -1,3 +1,11 @@
 import lyricsgenius
 
-genius = lyricsgenius.Genius()
+
+with open("credentials.txt", "r") as f:
+    token = f.read()
+
+genius = lyricsgenius.Genius(token)
+
+artist = genius.search_artist("The Beatles", max_songs=1, sort="title")
+
+print(artist.songs)
